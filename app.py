2846f7ee495357a8155729e7f0e761f4d8e39ce3
nolanding <deekshachandvani@gmail.com>
2020-09-19 03:34:48 +0530

    added logger, test_cases, database connectivity

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
 from flask import Flask, render_template, request
+from pymongo import MongoClient 
 
 # import our OCR function
-from ocr import ocr_core
+from ocr import ocr_core, logger
 
 # define a folder to store and later serve the images
 UPLOAD_FOLDER = os.getcwd() + '/images/'
@@ -11,6 +12,9 @@ UPLOAD_FOLDER = os.getcwd() + '/images/'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 app = Flask(__name__)
+client = MongoClient("mongodb://localhost:27017/")
+database = client['site_users']  
+collection = database.pan_details  
 
 # function to check the file extension
 def allowed_file(filename):
@@ -20,38 +24,43 @@ def allowed_file(filename):
 # route and function to handle the home page
 @app.route('/')
 def home_page():
-    print('home_page')
+    logger.info('home_page')
     return render_template('index.html')
 
 # route and function to handle the upload page
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_page():
-    print('main_block')
     if request.method == 'POST':
         # check if there is a file in the request
         if 'file' not in request.files:
+            logger.info('File not found')
             return render_template('error.html', msg='No file selected')
         file = request.files['file']
         # if no file is selected
         if file.filename == '':
+            logger.info('File not found')
             return render_template('error.html', msg='No file selected')
         #improper file format
         if not allowed_file(file.filename):
+            logger.info('Please check the file extension. Only {} allowed'.format(ALLOWED_EXTENSIONS))
             return render_template('error.html', msg='Please check the file extension. Only {} allowed'.format(ALLOWED_EXTENSIONS))
         #true case
         if file and allowed_file(file.filename):
 
             path = os.path.join(UPLOAD_FOLDER, file.filename)
             file.save(path)
+            logger.info('Copy of file saved')
 
-            # call the OCR function on it
-            extracted_text = ocr_core(path)
+            # call the OCR function on it and save or user details from mongodb
+            extracted_text = ocr_core(path, collection)
+            if(len(extracted_text) == 0):
+                return render_template('error.html', msg='Unable to read image.')
+
 
             # extract the text and display it
             return render_template('result.html',
                                    msg='Successfully Processed',
-                                   extracted_text=extracted_text,
-                                   img_src=UPLOAD_FOLDER + file.filename)
+                                   extracted_text=extracted_text)
     elif request.method == 'GET':
         return render_template('upload.html')
 
--- ocr.py
+++ ocr.py
@@ -4,20 +4,27 @@ except ImportError:
     import Image
 import pytesseract
 import cv2
+import logging
+from pymongo import MongoClient 
+
+logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(asctime)s - %(lineno)d - %(message)s ')
+logger = logging.getLogger('app')
+logger.setLevel(logging.DEBUG) 
 
 
-def ocr_core(file):
+
+def ocr_core(file, collection):
     """
     This function will handle the core OCR processing of images.
     """
     # text = pytesseract.image_to_string(Image.open(filename))  # We'll use Pillow's Image class to open the image and pytesseract to detect the string in the image
     # return text
-    print(file)
-    print('im in ocr')
+    logger.info(file)
     image = cv2.imread(file)
     image = cv2.resize(image, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_CUBIC)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     ret,thresh = cv2.threshold(gray,127,255,0)
+    logger.info('Image extracted')
     # # noise
     gray = cv2.medianBlur(thresh, 5)
     # apply OCR to it
@@ -27,16 +34,38 @@ def ocr_core(file):
     data = pytesseract.image_to_string(Image.open(file_name)).split('\n')
     data = list(filter(lambda x: x !='', data))
     data = list(filter(lambda x: x !=' ', data))
+    logger.info('data : ' + str(data))
     pan_details = {}
-    if(len(data)>=6):
-    	pan_details['fathers_name']	 = data[3]
-    	pan_details['name']	= data[2]
-    	pan_details['dob']	= data[4]
-    	pan_details['pan_number'] = data[6].split(' ')[0]
-    print(pan_details)
-    return pan_details
+    user_details = {}
+    try:
+        # if('Permanent Account Number' not in data): #if document is other than pancard
+        #     return pan_details
+
+        if(len(data)>=6):
+            pan_details['fathers_name']	= data[3]
+            pan_details['name']	= data[2]
+            pan_details['dob']	= data[4]
+            pan_details['pan_number'] = data[6].split(' ')[0]
+        logger.info('pan_details : ' + str(pan_details))
+        user_details = {'name' : pan_details['name'],  'fathers_name' : pan_details['fathers_name'], 
+                    'dob' :pan_details['dob'], '_id' : pan_details['pan_number']}
+    except Exception as e:
+        logger.debug('Unable to read image.')
+        return pan_details
+    try:                 
+        collection.insert_one(user_details) 
+        return pan_details
+    except Exception as e:
+        logger.error(e)
+        cursor = collection.find({'_id':pan_details['pan_number']})
+        for a in cursor:
+            return a
 
 
 if __name__ == '__main__':
-	file_name = input()
-	print(ocr_core(file_name))
+    file_name = input()
+    client = MongoClient("mongodb://localhost:27017/")
+    database = client['site_users']  
+    collection = database.pan_details  
+    print(ocr_core(file_name,collection))
+
